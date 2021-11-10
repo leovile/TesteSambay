9f8ba2008d4a82a6e50440b681b50cefdfe2204d
Leonardo Vilela <leonardo.vilela@leadsoft.inf.br>
2021-11-10 19:22:46 -0300

    Added readme

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,16 @@
+import { Fragment } from 'react'
+
+import * as S from './styles'
+
+import GlobalStyles from 'styles/global'
+
+const Main = () => {
+  return (
+    <Fragment>
+      <S.Container>Hello</S.Container>
+      <GlobalStyles />
+    </Fragment>
+  )
+}
+
+export default Main
