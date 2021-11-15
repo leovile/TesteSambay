cf8244d1c760eb5308f39b13c41dff3d1caa92eb
Leonardo Vilela <leonardo.vilela@leadsoft.inf.br>
2021-11-15 18:31:47 -0300

    Fixed type error warning

--- src/pages/DetailedProduct/index.tsx
+++ src/pages/DetailedProduct/index.tsx
@@ -1,5 +1,12 @@
 import ProductDetails from 'components/ProductDetails';
+import { NextPage } from 'next';
 
-export default function DetailedProduct() {
-  return <ProductDetails />;
-}
+import * as T from './types';
+
+const DetailedProduct: NextPage<T.ProductCardComponentProps> = ({
+  product
+}) => {
+  return <ProductDetails product={product} />;
+};
+
+export default DetailedProduct;
